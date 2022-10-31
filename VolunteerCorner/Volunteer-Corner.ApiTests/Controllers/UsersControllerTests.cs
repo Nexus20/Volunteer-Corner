619f2af01612981876689efe7fc167ac2d06ae5d
Олексій Косинський <oleksii.kosynskyi@nure.ua>
2022-10-31 16:15:52 +0200

    Закрыл тесты на контролерах в API

--- VolunteerCorner/Volunteer-Corner.ApiTests/Controllers/UsersControllerTests.cs
+++ VolunteerCorner/Volunteer-Corner.ApiTests/Controllers/UsersControllerTests.cs
@@ -1,14 +1,17 @@
-﻿//using FluentAssertions;
-//using Microsoft.AspNetCore.Http;
-//using Microsoft.AspNetCore.Mvc;
-//using Moq;
-//using NUnit.Framework;
-//using Volunteer_Corner.API.Controllers;
-//using Volunteer_Corner.Business.Interfaces;
-//using Volunteer_Corner.Business.Models.Requests;
-//using Volunteer_Corner.Business.Models.Results;
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using NUnit.Framework;
+using Volunteer_Corner.API.Controllers;
+using Volunteer_Corner.Business.Interfaces;
+using Volunteer_Corner.Business.Models.Requests;
+using Volunteer_Corner.Business.Models.Results;
 
-//namespace Volunteer_Corner.ApiTests.Controllers;
+namespace Volunteer_Corner.ApiTests.Controllers;
+
+using NUnit.Framework;
+using Volunteer_Corner.Business.Interfaces;
 
 [TestFixture]
 public class UsersControllerTests
@@ -16,13 +19,13 @@ public class UsersControllerTests
     private Mock<IUserService> _mockedUserService = null!;
     private Mock<ISignInService> _mockedSignInService = null!;
     private UsersController _usersController = null!;
-    
-//    [OneTimeSetUp]
-//    public void OneTimeSetUp()
-//    {
-//        // This method is called BEFORE ANY OF THE tests will be launched
-//        // Do common initializing stuff
-//    }
+
+    [OneTimeSetUp]
+    public void OneTimeSetUp()
+    {
+        // This method is called BEFORE ANY OF THE tests will be launched
+        // Do common initializing stuff
+    }
 
     [SetUp]
     public void SetUp()
@@ -34,24 +37,68 @@ public class UsersControllerTests
         _usersController = new UsersController(_mockedUserService.Object, _mockedSignInService.Object);
     }
 
-//    [Test]
-//    public async Task Register_WhenUserRegisteredSuccessfully_ReturnsStatus201()
-//    {
-//        // Arrange
-//        var expectedResult = new RegisterResult()
-//        {
-//            Id = "Some registered user id"
-//        };
+    [Test]
+    public async Task Register_WhenUserRegisteredSuccessfully_ReturnsStatus201()
+    {
+        // Arrange
+        var expectedResult = new RegisterResult()
+        {
+            Id = "Some registered user id"
+        };
 
-//        _mockedUserService.Setup(m => m.RegisterAsync(It.IsAny<RegisterRequest>()))
-//            .ReturnsAsync(expectedResult);
-        
-//        // Act
-//        var actualResult = await _usersController.Register(Mock.Of<RegisterRequest>()) as ObjectResult;
+        _mockedUserService.Setup(m => m.RegisterAsync(It.IsAny<RegisterRequest>()))
+            .ReturnsAsync(expectedResult);
 
-//        // Assert
-//        actualResult.Should().NotBeNull();
-//        actualResult!.Value.Should().BeEquivalentTo(expectedResult);
-//        actualResult.StatusCode.Should().Be(StatusCodes.Status201Created);
-//    }
-//}
+        // Act
+        var actualResult = await _usersController.Register(Mock.Of<RegisterRequest>()) as ObjectResult;
+
+        // Assert
+        actualResult.Should().NotBeNull();
+        actualResult!.Value.Should().BeEquivalentTo(expectedResult);
+        actualResult.StatusCode.Should().Be(StatusCodes.Status201Created);
+    }
+
+    [Test]
+    public async Task Login_WhenUserLoginSuccessfully_ReturnsStatus200()
+    {
+        // Arrange
+        var expectedResult = new LoginResult()
+        {
+            IsAuthSuccessful = true
+        };
+
+        _mockedSignInService.Setup(m => m.SignInAsync(It.IsAny<LoginRequest>()))
+            .ReturnsAsync(expectedResult);
+
+        // Act
+        var actualResult = await _usersController.Login(Mock.Of<LoginRequest>()) as ObjectResult;
+
+        // Assert
+        actualResult.Should().NotBeNull();
+        actualResult!.Value.Should().BeEquivalentTo(expectedResult);
+        actualResult.StatusCode.Should().NotBe(StatusCodes.Status401Unauthorized);
+        actualResult.StatusCode.Should().Be(StatusCodes.Status200OK);
+    }
+
+
+    [Test]
+    public async Task Login_WhenUserLoginFailed_ReturnsStatus401()
+    {
+        // Arrange
+        var expectedResult = new LoginResult()
+        {
+            IsAuthSuccessful = false
+        };
+
+        _mockedSignInService.Setup(m => m.SignInAsync(It.IsAny<LoginRequest>()))
+            .ReturnsAsync(expectedResult);
+
+        // Act
+        var actualResult = await _usersController.Login(Mock.Of<LoginRequest>()) as ObjectResult;
+
+        // Assert
+        actualResult.Should().NotBeNull();
+        actualResult!.Value.Should().BeEquivalentTo(expectedResult);
+        actualResult.StatusCode.Should().Be(StatusCodes.Status401Unauthorized);
+    }
+}
---
+++ VolunteerCorner/Volunteer-Corner.BusinessTests/Services/HelpRequestServiceTests.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using FluentAssertions;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Moq;
+using NUnit.Framework;
+using Volunteer_Corner.Business.Infrastructure;
+using Volunteer_Corner.Business.Interfaces;
+using Volunteer_Corner.Business.Models.Requests;
+using Volunteer_Corner.Business.Models.Results;
+using Volunteer_Corner.Business.Services;
+using Volunteer_Corner.Data.Entities;
+using Volunteer_Corner.Data.Entities.Identity;
+using Volunteer_Corner.Data.Interfaces;
+
+namespace Volunteer_Corner.BusinessTests.Services
+{
+    [TestFixture]
+    public class HelpRequestServiceTests
+    {
+        private IConfiguration _configuration;
+        private IMapper _mapper;
+        private ISignInService _signInService;
+        private Mock<UserManager<User>> _mockedUserManager;
+        private JwtHandler _jwtHandler;
+        private Mock<IRepository<HelpRequest>> _helpRequestRepository;
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            // This method is called BEFORE ANY OF THE tests will be launched
+            // Do common initializing stuff
+            _mapper = UnitTestsHelper.GetMapper();
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            // This method is called BEFORE EACH OF THE tests will be launched
+            // Do initializing stuff that needs to be applied before each test
+            _mockedUserManager = UnitTestsHelper.GetUserManagerMock();
+            _configuration = UnitTestsHelper.GetConfiguration();
+            _jwtHandler = new JwtHandler(_configuration, _mockedUserManager.Object);
+            _helpRequestRepository = new Mock<IRepository<HelpRequest>>();
+            _signInService = new SignInService(_mockedUserManager.Object, _jwtHandler);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            // This method is called AFTER EVERY test had been launched
+            // Do all stuff that needs to be applied after unit tests will end its work 
+
+        }
+
+        [OneTimeTearDown]
+        public void OneTimeTearDown()
+        {
+            // This method is called AFTER ANY OF THE tests had been launched
+            // Do all stuff that needs to be applied after unit tests will end its work 
+        }
+
+
+        // Please use Triple A convention (Arrange, Act, Assert)
+        // Naming convention
+        // <MethodName>_<WhenSomeActionOccurs>_<DoSomeExpectedResult>
+
+
+        //[Test]
+        //public async Task RegisterAsync_WhenAccountTypeIsInvalid_ThrowsValidationException()
+        //{
+        //    // Arrange
+        //    var predicate = CreateFilterPredicate(request);
+
+
+        //    _mockedUserManager.Setup(m => m.FindByNameAsync(It.IsAny<string>()))
+        //        .ReturnsAsync(new User());
+
+        //    // Act
+        //    var action = async () => { await _userService.RegisterAsync(request); };
+
+        //    // Assert
+        //    await action.Should().ThrowAsync<ValidationException>()
+        //        .WithMessage(expectedMessage);
+        //}
+    }
+    
+}
