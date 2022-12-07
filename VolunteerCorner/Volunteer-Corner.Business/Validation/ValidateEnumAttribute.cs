bbb8591ed63bf95a9043808b27f2ee3e4fe54733
Yevhen Chubarov <40770350+Nexus20@users.noreply.github.com>
2022-12-08 00:56:37 +0200

    Updated connection strings

--- VolunteerCorner/Volunteer-Corner.Business/Validation/ValidateEnumAttribute.cs
+++ VolunteerCorner/Volunteer-Corner.Business/Validation/ValidateEnumAttribute.cs
@@ -6,10 +6,11 @@ public class ValidateEnumAttribute : ValidationAttribute
 {
     protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
     {
-        if(value != null && value.GetType().IsEnum && !Enum.IsDefined(value.GetType(), value))
+        if (value != null && value.GetType().IsEnum && !Enum.IsDefined(value.GetType(), value))
         {
             return new ValidationResult($"Provided value is not defined in enum {value.GetType().Name}");
         }
+
         return ValidationResult.Success;
     }
 }
